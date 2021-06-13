f9ca0e84aa91a79ae0bca9be8ba07b7c8a1c4fea
Davsav <davidbyu6@gmail.com>
2021-06-12 19:08:51 -0600

    finished category and product routes

--- models/index.js
+++ models/index.js
@@ -6,26 +6,24 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: ''
+  foreignKey: 'category_id'
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: ''
+  foreignKey: 'category_id'
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: '',
-  foreignKey: ''
+  foreignKey: 'product_id'
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  as: '',
-  through: ''
+  through: 'tag_id'
 });
 
 module.exports = {
